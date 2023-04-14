85223f7c8f2a8ba003fe9a8ba724ab5368b14a4e
git <git@users.noreply.github.com>
2023-04-14 17:59:13 +0800

    update FileManager

--- SwiftExtension/Classes/FileManager+ext.swift
+++ SwiftExtension/Classes/FileManager+ext.swift
@@ -17,6 +17,12 @@ extension FileManager {
         return documentsDirectory
     }
     
+    public static func applicationDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .applicationDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+        return documentsDirectory
+    }
+    
     public static func libraryDirectory() -> URL {
         let paths = FileManager.default.urls(for: .libraryDirectory, in: .userDomainMask)
         let documentsDirectory = paths[0]
@@ -59,6 +65,14 @@ extension FileManager {
         return false
     }
     
+    public static func renameWith(fileURL:URL,newFileURL:URL){
+        do {
+            try FileManager.default.moveItem(at: fileURL, to: newFileURL)
+        } catch {
+            print(error)
+        }
+    }
+    
     public static func removeWith(fileURL:URL){
          
         //file
@@ -84,9 +98,30 @@ extension FileManager {
             for filePath in filePaths {
                 try FileManager.default.removeItem(atPath: fileURL.appendingPathComponent(filePath).path)
             }
+            try FileManager.default.removeItem(at: fileURL)
         } catch {
             print("Could not remove \(fileURL): \(error)")
         }
+    }
+    
+    public static func listAllFile(path: URL, extensionWanted: String? = nil) -> (names : [String], paths : [URL]) {
+ 
+        do {
+            try FileManager.default.createDirectory(atPath: path.relativePath, withIntermediateDirectories: true)
+            // Get the directory contents urls (including subfolders urls)
+            let directoryContents = try FileManager.default.contentsOfDirectory(at: path, includingPropertiesForKeys: nil, options: [])
+
+            // if you want to filter the directory contents you can do like this:
+            let FilesPath = directoryContents.filter{ extensionWanted == nil ? true : $0.pathExtension == extensionWanted }
+            let FileNames = FilesPath.map{ $0.deletingPathExtension().lastPathComponent }
+
+            return (names : FileNames, paths : FilesPath);
+
+        } catch {
+            print(error.localizedDescription)
+        }
+
+        return (names : [], paths : [])
     }
 
 }
